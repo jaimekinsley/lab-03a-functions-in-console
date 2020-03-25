7e773824e022b2a64a6f9ac82062183037143555
Jaime Sanders <jaimelyn517@gmail.com>
2020-03-25 16:25:16 -0700

    changed my array to six items

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const Kaya = {name: 'Kaya', age: 7, weight: 30, color: 'black'};
 const Elle = {name: 'Elle', age: 1, weight: 10, color: 'brown'};
 const Layla = {name: 'Layla', age: 3, weight: 25, color: 'brown'}
 const Wicket = {name: 'Wicket', age: 5, weight: 12, color: 'brown'};
+const Louie = {name: 'Louie', age: 12, weight: 35, color: 'black'}
 
-const dogs = [KC, Kaya, Elle, Layla, Wicket];
+const dogs = [KC, Kaya, Elle, Layla, Wicket, Louie];
 
 
 for(let i = 0; i < dogs.length; i++) {
     const dog = dogs[i];
     console.log('dog at index', i, dog);
 }
+
+function logOutThings(){
+  console.log('Here are your', dogs.length, 'things!');  
+} 
+
+logOutThings();
