b710cd7e5027042c8a37be8bf22ec41ccd75482a
Jaime Sanders <jaimelyn517@gmail.com>
2020-03-25 16:54:26 -0700

    Stretch 5- separated dogs by colors

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-const KC = {name: 'KC', age: 2, weight: 15, color: 'black'};
-const Kaya = {name: 'Kaya', age: 7, weight: 30, color: 'black'};
-const Elle = {name: 'Elle', age: 1, weight: 10, color: 'brown'};
-const Layla = {name: 'Layla', age: 3, weight: 25, color: 'brown'}
-const Wicket = {name: 'Wicket', age: 5, weight: 12, color: 'brown'};
-const Louie = {name: 'Louie', age: 12, weight: 35, color: 'black'}
+const kc = {name: 'KC', age: 2, weight: 15, color: 'black'};
+const kaya = {name: 'Kaya', age: 7, weight: 30, color: 'black'};
+const elle = {name: 'Elle', age: 1, weight: 10, color: 'brown'};
+const layla = {name: 'Layla', age: 3, weight: 25, color: 'brown'}
+const wicket = {name: 'Wicket', age: 5, weight: 12, color: 'brown'};
+const louie = {name: 'Louie', age: 12, weight: 35, color: 'black'}
 
-const dogs = [KC, Kaya, Elle, Layla, Wicket, Louie];
+const dogs = [kc, kaya, elle, layla, wicket, louie];
+
 
 
 function logOutThings(){
   console.log('Here are your', dogs.length, 'things!'); 
   for(let i = 0; i < dogs.length; i++) {
     const dog = dogs[i];
-    console.log('dog at index', i, dog);
+    if(dog.color === 'black'){
+        console.log('black dog');
+    }
+        else if(dog.color === 'brown'){
+            console.log('brown dog');
+        }
+        console.log(dog);
 } 
 } 
 
 logOutThings();
+
